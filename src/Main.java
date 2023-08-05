dffc756cdd0bb63bc0b8d18125e69737f04b5f33
Cheruiyot Collins <kelvincollins86@gmail.com>
2023-08-05 19:59:10 +0300

    Added Scanner for user input

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,34 @@
 import java.lang.Math;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-        double loanAmount = 10000.0;
-        int loanTerm = 12;
-        double interestRate = 5;
-        int repaymentFrequency = 1;
+        System.out.printf("<<<<<<<<Welcome to Pezesha Loan Repayment Calculator. Press enter to continue>>>>");
+        String[] input = new String[4];
+        in.nextLine(); //consuming the <enter> from input above
+
+
+
+        System.out.println("Please enter loan amount");
+        input[0] = in.nextLine();
+        System.out.println("Please enter loan term(in months)");
+        input[1] = in.nextLine();
+        System.out.println("Please enter Interest rate (per year)");
+        input[2] = in.nextLine();
+        System.out.println("Please enter Repayment frequency: For monthly enter 1, for bi-monthly enter 2 for  weekly enter 4)");
+        input[3] = in.nextLine();
+
+
+        System.out.printf("\n.......Please wait while we calculate your repayments......\n");
+
+        //end collo
+
+        double loanAmount = Double.parseDouble(input[0]);
+        int loanTerm = Integer.parseInt(input[1]);
+        double interestRate = Double.parseDouble(input[2]);
+        int repaymentFrequency = Integer.parseInt(input[3]);;
         int compoundInterestInterval = 0;
 
         switch (repaymentFrequency) {
@@ -42,8 +64,10 @@ public class Main {
         System.out.println("                  REPAYMENT TABLE                          ");
         System.out.println(" __________________________________________________________");
         System.out.printf("%5s %10s %10s %8s", "PRINCIPAL AMOUNT", "INTEREST", "REPAYMENT", " OUTSTANDING AMOUNT");
-
+          int count1=0;
         while (totalInterest + loanAmount > 0) {
+            count1++;
+
             System.out.println();
             System.out.println(" __________________________________________________________");
             System.out.format("%7s %18s %7s %14s", Math.floor(loanAmount * 100) / 100, Math.floor(totalInterest * 100) / 100, Math.floor(monthlyRepayment * 100) / 100, Math.floor((loanAmount + totalInterest) * 100) / 100);
@@ -74,14 +98,16 @@ public class Main {
             count++;
 
         }
+
         System.out.println("");
+        System.out.println(count1);
         System.out.println("");
         System.out.println("                  REPAYMENT SUMMARY TABLE                          ");
         System.out.println(" __________________________________________________________");
-        System.out.printf("%5s %20s", "TOTAL AMOUNT REPAID", "TOTAL INTEREST");
+        System.out.printf("%5s %22s %20s", "PRINCIPAL_AMOUNT","TOTAL_AMOUNT_REPAID", "TOTAL_INTEREST");
         System.out.println();
         System.out.println(" __________________________________________________________");
-        System.out.format("%7s %25s", Math.floor(totalPayments * 100) / 100, Math.floor((totalPayments-principleAmount) * 100) / 100);
+        System.out.format("%7s %20s %25s ", Math.floor(principleAmount * 100) / 100,Math.floor(totalPayments * 100) / 100, Math.floor((totalPayments-principleAmount) * 100) / 100);
 
     }
 
