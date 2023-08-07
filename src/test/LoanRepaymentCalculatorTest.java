50262d22c2446408cbbe756c4c60c7ccbae55a97
Cheruiyot Collins <kelvincollins86@gmail.com>
2023-08-07 14:19:35 +0300

    Added test Class

---
+++ src/test/LoanRepaymentCalculatorTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class LoanRepaymentCalculatorTest {
+    @Test
+    public void loanRepaymentCalculator(double loanAmount, int loanTerm, double interestRate, int repaymentFrequency, int compoundInterestInterval) {
+
+
+    }
+}
