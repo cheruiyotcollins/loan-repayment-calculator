002c06818aa29ccf5e53f6aeeb8cde11b5204cc8
Cheruiyot Collins <kelvincollins86@gmail.com>
2023-08-07 12:57:07 +0300

    added multithreading

---
+++ src/LoanRepaymentCalculator.java
@@ -0,0 +1,73 @@
+public class LoanRepaymentCalculator extends Thread{
+    double loanAmount = 0.0;
+    int loanTerm = 0;
+    double interestRate =0.0;
+    int repaymentFrequency =0;
+    int compoundInterestInterval = 0;
+    LoanRepaymentCalculator(){
+    }
+    LoanRepaymentCalculator(double loanAmount, int loanTerm, double interestRate, int repaymentFrequency, int compoundInterestInterval){
+        this.loanAmount=loanAmount;
+        this.loanTerm=loanTerm;
+        this.interestRate=interestRate;
+        this.repaymentFrequency=repaymentFrequency;
+        this.compoundInterestInterval=compoundInterestInterval;
+    }
+
+
+    public void loanRepaymentCalculator(double loanAmount, int loanTerm, double interestRate, int repaymentFrequency, int compoundInterestInterval) {
+
+        double totalPayments = 0.0;
+        int count = 0;
+        double principleAmount = loanAmount;
+        int repaymentTimes = repaymentFrequency * loanTerm;
+        double totalInterest = loanAmount * interestRate / 100;
+        double monthlyRepayment = (totalInterest + loanAmount) / repaymentTimes;
+        System.out.println();
+        System.out.println("                  REPAYMENT TABLE                          ");
+        System.out.println(" __________________________________________________________");
+        System.out.printf("%5s %10s %10s %8s", "PRINCIPAL AMOUNT", "INTEREST", "REPAYMENT", " OUTSTANDING AMOUNT");
+
+        while (totalInterest + loanAmount > 0) {
+            System.out.println();
+            System.out.println(" __________________________________________________________");
+            System.out.format("%7s %18s %7s %14s", Math.floor(loanAmount * 100) / 100, Math.floor(totalInterest * 100) / 100, Math.floor(monthlyRepayment * 100) / 100, Math.floor((loanAmount + totalInterest) * 100) / 100);
+            totalPayments += monthlyRepayment;
+
+            if (totalInterest > 0) {
+                totalInterest -= monthlyRepayment;
+                if (totalInterest < 0) {
+                    loanAmount += totalInterest;
+                    totalInterest = 0;
+                    if (loanAmount < 0) {
+                        totalPayments += loanAmount;
+                        break;
+                    }
+                }
+                continue;
+            }
+            loanAmount -= monthlyRepayment;
+            if (loanAmount < 0 && totalInterest == 0) {
+                totalPayments += loanAmount;
+                break;
+            }
+
+            if (count == compoundInterestInterval) {
+                count = 0;
+                totalInterest = (totalInterest + loanAmount) * interestRate / 100;
+            }
+            count++;
+
+        }
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("                  REPAYMENT SUMMARY TABLE                          ");
+        System.out.println(" __________________________________________________________");
+        System.out.printf("%5s %22s %20s", "PRINCIPAL_AMOUNT", "TOTAL_AMOUNT_REPAID", "TOTAL_INTEREST");
+        System.out.println();
+        System.out.println(" __________________________________________________________");
+        System.out.format("%7s %20s %25s ", Math.floor(principleAmount * 100) / 100, Math.floor(totalPayments * 100) / 100, Math.floor((totalPayments - principleAmount) * 100) / 100);
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -24,18 +24,27 @@ public class Main {
         int repaymentFrequency = Integer.parseInt(input[3]);
         int compoundInterestInterval = 0;
 
+        LoanRepaymentCalculator loanRepaymentCalculator;
+
+
         switch (repaymentFrequency) {
             case 1:
                 compoundInterestInterval = 12;
-                loanRepaymentCalculator(loanAmount, loanTerm, interestRate, repaymentFrequency, compoundInterestInterval);
+                loanRepaymentCalculator=new LoanRepaymentCalculator();
+                loanRepaymentCalculator.loanRepaymentCalculator(loanAmount, loanTerm, interestRate, repaymentFrequency, compoundInterestInterval);
+                loanRepaymentCalculator.start();
                 break;
             case 2:
                 compoundInterestInterval = 26;
-                loanRepaymentCalculator(loanAmount, loanTerm, interestRate, repaymentFrequency, compoundInterestInterval);
+                loanRepaymentCalculator=new LoanRepaymentCalculator();
+                loanRepaymentCalculator.loanRepaymentCalculator(loanAmount, loanTerm, interestRate, repaymentFrequency, compoundInterestInterval);
+                loanRepaymentCalculator.start();
                 break;
             case 4:
                 compoundInterestInterval = 52;
-                loanRepaymentCalculator(loanAmount, loanTerm, interestRate, repaymentFrequency, compoundInterestInterval);
+                loanRepaymentCalculator=new LoanRepaymentCalculator();
+                loanRepaymentCalculator.loanRepaymentCalculator(loanAmount, loanTerm, interestRate, repaymentFrequency, compoundInterestInterval);
+                loanRepaymentCalculator.start();
                 break;
             default:
                 System.out.println("Invalid repayment frequency");
@@ -45,60 +54,6 @@ public class Main {
 
     }
 
-    static void loanRepaymentCalculator(double loanAmount, int loanTerm, double interestRate, int repaymentFrequency, int compoundInterestInterval) {
 
-        double totalPayments = 0.0;
-        int count = 0;
-        double principleAmount = loanAmount;
-        int repaymentTimes = repaymentFrequency * loanTerm;
-        double totalInterest = loanAmount * interestRate / 100;
-        double monthlyRepayment = (totalInterest + loanAmount) / repaymentTimes;
-        System.out.println();
-        System.out.println("                  REPAYMENT TABLE                          ");
-        System.out.println(" __________________________________________________________");
-        System.out.printf("%5s %10s %10s %8s", "PRINCIPAL AMOUNT", "INTEREST", "REPAYMENT", " OUTSTANDING AMOUNT");
-
-        while (totalInterest + loanAmount > 0) {
-            System.out.println();
-            System.out.println(" __________________________________________________________");
-            System.out.format("%7s %18s %7s %14s", Math.floor(loanAmount * 100) / 100, Math.floor(totalInterest * 100) / 100, Math.floor(monthlyRepayment * 100) / 100, Math.floor((loanAmount + totalInterest) * 100) / 100);
-            totalPayments += monthlyRepayment;
-
-            if (totalInterest > 0) {
-                totalInterest -= monthlyRepayment;
-                if (totalInterest < 0) {
-                    loanAmount += totalInterest;
-                    totalInterest = 0;
-                    if (loanAmount < 0) {
-                        totalPayments += loanAmount;
-                        break;
-                    }
-                }
-                continue;
-            }
-            loanAmount -= monthlyRepayment;
-            if (loanAmount < 0 && totalInterest == 0) {
-                totalPayments += loanAmount;
-                break;
-            }
-
-            if (count == compoundInterestInterval) {
-                count = 0;
-                totalInterest = (totalInterest + loanAmount) * interestRate / 100;
-            }
-            count++;
-
-        }
-
-        System.out.println("");
-        System.out.println("");
-        System.out.println("                  REPAYMENT SUMMARY TABLE                          ");
-        System.out.println(" __________________________________________________________");
-        System.out.printf("%5s %22s %20s", "PRINCIPAL_AMOUNT", "TOTAL_AMOUNT_REPAID", "TOTAL_INTEREST");
-        System.out.println();
-        System.out.println(" __________________________________________________________");
-        System.out.format("%7s %20s %25s ", Math.floor(principleAmount * 100) / 100, Math.floor(totalPayments * 100) / 100, Math.floor((totalPayments - principleAmount) * 100) / 100);
-
-    }
 
 }
